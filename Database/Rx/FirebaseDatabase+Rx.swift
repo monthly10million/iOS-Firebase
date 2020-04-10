d0e74ca3ec2d94e10142e89d85dfb9a60579d3d7
monibu1548 <monibu1548@gmail.com>
2020-04-10 21:47:35 +0900

    Update firebase database fetch logic

--- Database/Rx/FirebaseDatabase+Rx.swift
+++ Database/Rx/FirebaseDatabase+Rx.swift
@@ -12,19 +12,6 @@ import RxCocoa
 extension FirebaseDatabase: ReactiveCompatible {}
 
 extension Reactive where Base: FirebaseDatabase {
-    func loadFirebaseObject<D: FirebaseObject>(path: String, type: D.Type) -> Single<D?> {
-        let single = Single<D?>.create { [weak base] single in
-            base?.loadFirebaseObject(path: path, type: type) { object in
-                guard let object = object else {
-                    return single(.error(NSError()))
-                }
-                return single(.success(object))
-            }
-            return Disposables.create {}
-        }
-        return single
-    }
-
     func loadObject<D: Decodable>(path: String, type: D.Type) -> Single<D?> {
         let single = Single<D?>.create { [weak base] single in
             base?.loadObject(path: path, type: type) { object in
@@ -35,22 +22,22 @@ extension Reactive where Base: FirebaseDatabase {
         return single
     }
 
-    func loadFirebaseObjects<D: FirebaseObject>(path: String, query: FirebaseDatabaseQuery =  FirebaseDatabaseQuery(), type: [D].Type) -> Single<[D]> {
-        let single = Single<[D]>.create { [weak base] single in
-            base?.loadFirebaseObjects(path: path, query: query, type: type) { objects in
-                return single(.success(objects))
+    func loadFirebaseObject<D: FirebaseObject>(path: String, type: D.Type) -> Single<D?> {
+        let single = Single<D?>.create { [weak base] single in
+            base?.loadFirebaseObject(path: path, type: type) { object in
+                guard let object = object else {
+                    return single(.success(nil))
+                }
+                return single(.success(object))
             }
             return Disposables.create {}
         }
         return single
     }
-
-    func loadObjects<D: Decodable>(path: String, query: FirebaseDatabaseQuery? = nil, type: [D].Type) -> Single<[D]> {
+    
+    func loadFirebaseObjects<D: FirebaseObject>(path: String, query: FirebaseDatabaseQuery =  FirebaseDatabaseQuery(), type: [D].Type) -> Single<[D]> {
         let single = Single<[D]>.create { [weak base] single in
-            base?.loadObjects(path: path, query: query, type: type) { objects in
-                guard let objects = objects else {
-                    return single(.error(NSError()))
-                }
+            base?.loadFirebaseObjects(path: path, query: query, type: type) { objects in
                 return single(.success(objects))
             }
             return Disposables.create {}
@@ -67,7 +54,7 @@ extension Reactive where Base: FirebaseDatabase {
         base.setObject(path: path, object: object)
         return .just(Void())
     }
-    
+
     func addFirebaseObject(path: String, object: FirebaseObject) -> Single<String> {
         let childID = base.addFirebaseObject(path: path, object: object)
         return .just(childID)
